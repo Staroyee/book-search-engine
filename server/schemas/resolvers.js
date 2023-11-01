c743aa9d7a9b5d1789fd3a0d3cf4176b789404d6
Staroyee <danmasefield@gmail.com>
2023-11-02 09:56:34 +1030

    Created resolvers for finding one user by id and creating a user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
-const { Book, User } = require('../models');
+const { AuthenticationError, signToken } = require("../utils/auth");
+const { Book, User } = require("../models");
 
 const resolvers = {
   Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).select("-__v -password");
+      }
+      throw AuthenticationError;
+    },
   },
   Mutation: {
+    addUser: async (_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { user, token };
+    },
   },
 };
 
